7eaf35a059d65869b2fc83e65f41249f0066e510
Lalo <ahottois@gmail.com>
2025-12-17 21:31:18 +0100

    update

--- firewall/Program.cs
+++ firewall/Program.cs
@@ -54,7 +54,43 @@ builder.Services.AddHostedService<DnsServerService>(provider => provider.GetRequ
 builder.Services.AddSingleton<PortMappingService>();
 builder.Services.AddHostedService<PortMappingService>(provider => provider.GetRequiredService<PortMappingService>());
 
-```
+// Packet Sniffer Service
+builder.Services.AddSingleton<IPacketSnifferService, PacketSnifferService>();
+
+// Add Controllers
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+});
+
+// Add Swagger/OpenAPI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+// Initialize Database
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<FirewallDbContext>();
+    db.Database.EnsureCreated();
+}
+
+app.Run();
 
 
 
